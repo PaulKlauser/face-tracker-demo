91ee1208ff0ab16f5da02daee083e75e8fa97fe6
Paul Klauser <paul.klauser@willowtreeapps.com>
2023-08-14 13:19:36 -0400

    Redeclare dependencies using version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 
 dependencies {
 
-    implementation("com.google.ar:core:1.38.0")
-    implementation("com.google.ar.sceneform.ux:sceneform-ux:1.17.1")
+    implementation(libs.ar.core)
+    implementation(libs.sceneform.ux)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
